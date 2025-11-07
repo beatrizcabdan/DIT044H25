9750dfa14cfa6bbe3e32b14a9ab1185aba0a8133
beacab <beatriz.cabrero.daniel@gmail.com>
2025-11-07 13:33:40 +0100

    Exercises for W46 (live-coding in class)

---
+++ src/W46Inheritance/Overloading1.java
@@ -0,0 +1,35 @@
+package W46Inheritance;
+
+class Mammal {
+    void makeSound() {
+        System.out.println("???");
+    }
+}
+
+class Cat extends Mammal {
+    @Override // you do not "really" need this
+    void makeSound() {
+        System.out.println("Marrameu");
+    }
+}
+
+class Human extends Mammal {
+    @Override
+    void makeSound() {
+        super.makeSound();
+        System.out.println("Hola!");
+    }
+}
+
+public class Overloading1 {
+    public static void main(String[] args) {
+        Mammal mammal = new Mammal();
+        Cat cat = new Cat();
+        Human human = new Human();
+
+        mammal.makeSound();
+        cat.makeSound();
+        human.makeSound();
+    }
+}
+
---
+++ src/W46Inheritance/Overloading2.java
@@ -0,0 +1,29 @@
+package W46Inheritance;
+
+class Reptile {
+    public void makeSound() {
+        System.out.println("Sssssss!");
+    }
+
+    public void makeSound(int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println("Sssssss!");
+        }
+    }
+}
+
+class Iguana extends Reptile {
+    @Override
+    public void makeSound() {
+        System.out.println("Fffffff!");
+    }
+}
+
+public class Overloading2 {
+    public static void main(String[] args) {
+        Iguana iguana = new Iguana();
+
+        iguana.makeSound();
+        iguana.makeSound(3); // this method is not overriden in iguana
+    }
+}
