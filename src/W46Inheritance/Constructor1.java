aaee9ebf84d4ff24392bb77e84af62d61fa1e4c9
beacab <beatriz.cabrero.daniel@gmail.com>
2025-11-11 16:03:49 +0100

    Exercises for W46 (live-coding in class)

---
+++ src/W46Inheritance/Constructor1.java
@@ -0,0 +1,32 @@
+package W46Inheritance;
+
+// EXERCISE: Define a Person class with two instance variables: String name, int age. Give it a constructor that initializes both fields. Then create a Student class that inherits from Person and write a constructor for Student that calls the superclass constructor using super(name, age).
+
+// SOLUTION:
+
+class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Student extends Person {
+    float averageGrade;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+}
+
+public class Constructor1 {
+    public static void main(String[] args) {
+        Person cecilia = new Person("Cecilia", 19);
+        Student mari = new Student("Mari", 20);
+        
+        Person tim = new Student("Tim", 18); // magic! we see this in W47!
+    }
+}
